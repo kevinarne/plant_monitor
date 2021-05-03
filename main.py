7cbaf09b7ebb39c208c8d8024b3ccd82db817621
kevin.arne <kevin.arne@gmail.com>
2021-05-03 22:17:12 +0100

    Added an integer conversion to the raw data off of the Arduino.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import time
 import serial
 #print([comport.device for comport in serial.tools.list_ports.comports()])
 #com = input("Which COM port is the Arduino connected to?")
-while True:
+def read_light():
 	with serial.Serial("/dev/ttyUSB0", 9600, timeout=10) as ser:
 		entry = ser.readline()
-		print(entry)
+		print(int(entry))
+	return entry
+
+while True:
 	usr = input("Would you like to continue?")
+	#time.sleep(3)
+	read_light()
 	if usr == "no":
 		break
-	
+
