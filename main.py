f24c36c36ac934a0fb6b6730732b6a86d37b4c11
kevin.arne <kevin.arne@gmail.com>
2021-05-05 19:25:26 +0100

    Added ability to log a light sensor reading to the database in main.py. Next steps are to add regular recording and clean up the code base. Input validation and credentials need to be sorted out.

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import time
 import serial
+import lightdbhandler as hndl
 #print([comport.device for comport in serial.tools.list_ports.comports()])
 #com = input("Which COM port is the Arduino connected to?")
 def read_light():
 	with serial.Serial("/dev/ttyUSB0", 9600, timeout=10) as ser:
 		entry = ser.readline()
 		print(int(entry))
-	return entry
+	return int(entry)
+with open("credentials","r") as f:
+	usr, pwd = f.read().strip().split()
+
+db = hndl.LightDBHandler("localhost", usr, pwd)
+
 while True:
 	usr = input("What would you like to do?")
 	if usr == "read sensor":
-		read_light()
+		entry = read_light()
+		db.add_value(entry)
 	elif usr == "exit":
 		break
 	#time.sleep(3)
