95e442f5467381c8b55b6e734a4e14abefe10f60
kevin.arne <kevin.arne@gmail.com>
2021-05-05 19:35:12 +0100

    Added checking for credentials file to main.py. Added default menu behavior when a valid input isn't used. Removed dbtest.py.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import time
 import serial
 import lightdbhandler as hndl
-#print([comport.device for comport in serial.tools.list_ports.comports()])
-#com = input("Which COM port is the Arduino connected to?")
+
 def read_light():
 	with serial.Serial("/dev/ttyUSB0", 9600, timeout=10) as ser:
 		entry = ser.readline()
 		print(int(entry))
 	return int(entry)
-with open("credentials","r") as f:
-	usr, pwd = f.read().strip().split()
+try:
+	with open("credentials","r") as f:
+		usr, pwd = f.read().strip().split()
+except:
+	print("Credentials failed to load, please consult setup/README.md for instructions on setting up the mysql database credentials.")
+	exit()
 
 db = hndl.LightDBHandler("localhost", usr, pwd)
 
@@ -20,4 +23,6 @@ while True:
 		db.add_value(entry)
 	elif usr == "exit":
 		break
+	else:
+		print("I'm sorry, thats not a valid options. The valid options are:\n - read sensor\n - exit")
 	#time.sleep(3)
