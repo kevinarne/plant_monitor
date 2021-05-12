a75fb993c692d639034e19d51b2736e9bdb49f20
Kevin Arne <kevin.arne@gmail.com>
2021-05-12 15:59:02 -0700

    Added a utility to pull the database and turn it into a csv to be easily read by Excel or Google Sheets.

---
+++ util/dbtocsv.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+# Get credentials
+try:
+    with open("credentials", "r") as f:
+        hostname, username, pwd = f.read().strip().split("\n")
+except:
+    print("Credentials failed to load")
+    exit()
+
+with mysql.connector.connect(host=hostname, user=username, password=pwd, database="lights") as db:
+    cur = db.cursor()
+    cur.execute("SELECT * FROM light_vals;")
+    data = cur.fetchall()
+    with open("db.csv","a") as f:
+        for entry in data:
+            f.write(str(entry[0]) + "," + str(entry[1]) + "," + str(entry[2]) + "\n")
