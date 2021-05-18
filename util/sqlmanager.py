625a5f1355f654892ac9f497cc1620d7a668fa27
kevin.arne <kevin.arne@gmail.com>
2021-05-18 12:23:13 -0700

    sqlmanager.py now creates all of the appropriate tables when run as main. Table creation seems to work, but I might consier adding more classes to handle individual tables. Database creation via sqlmanager.py needs to be refactored to work appropriately.

--- util/sqlmanager.py
+++ util/sqlmanager.py
@@ -1,7 +1,7 @@
 import pymysql
 #File for managing some basic mysql tasks in plainer English
 class MySqlManager:
-	def __init__(self, cred_path, db_name = None):
+	def __init__(self, cred_path, db_name = None, create_db = False):
 		print("initializing")
 		#read the credentials file
 		try:
@@ -32,18 +32,16 @@ class MySqlManager:
 	def create_table(self, tablename, primary, cols = None):
 		query = "CREATE TABLE " + tablename + " (" + primary.to_str()
 		if cols == None:
-			query += ", PRIMARY KEY(" + primary.name + "))"
-			print(query)
+			pass
 		else:
-			for num, col in enumerate(cols):
-				if num == 0:
-					pass
-				else:
-					pass
-				print(num)
-				print(col.to_str())
-		#query = "CREATE TABLE " + tablename + " (id INT NOT NULL AUTO_INCREMENT, val INT NOT NULL, datetime VARCHAR(30) NOT NULL, PRIMARY KEY(id))"
-		#self.execute_mysql(query,())
+			for col in cols:
+				query += ", " + col.to_str()
+		query += ", PRIMARY KEY(" + primary.name + "))"
+		print(query)
+		try:
+			self.execute_mysql(query,())
+		except:
+			print(tablename, "already exists.")
 
 	def create_db(self, db_name):
 		with pymysql.connect(host = self.host, user = self.username, password = self.pwd) as db:
@@ -72,8 +70,24 @@ class MySqlCol:
 		self.attrs = attrs
 	def to_str(self):
 		return str(self.name + " " + self.attrs)
+	def id_primary():
+		return MySqlCol("id", "INT NOT NULL AUTO_INCREMENT")
+
+class Table:
+	def __init__(self):
+		pass
 
 if __name__ == "__main__":
 	mngr = MySqlManager("credentials", db_name = "lights")
-	mngr.create_table("test_table", MySqlCol("id","ATTR1"), cols=[MySqlCol("test2","ATTR2"),MySqlCol("test3","ATTR3")])
-	pass
+	user_inp = input("Would you like to create the needed databases and tables (y/n)?")
+	if user_inp == "y":
+		#Create database
+		#Create light_vals table
+		mngr.create_table("light_vals", MySqlCol.id_primary(), cols = [MySqlCol("val","INT"), MySqlCol("datetime","VARCHAR(30)")])
+		#Create plants table
+		mngr.create_table("plants", MySqlCol.id_primary(), cols=[MySqlCol("nickname", "VARCHAR(40)"),MySqlCol("notes","VARCHAR(200)")])
+		#Create plant_events table
+		mngr.create_table("plant_events", MySqlCol.id_primary(), cols = [MySqlCol("code","INT NOT NULL"), MySqlCol("datetime","VARCHAR(30)"), MySqlCol("val","INT"), MySqlCol("plant","INT"), MySqlCol("notes","VARCHAR(140)")])
+		#Create event_codes table
+		mngr.create_table("event_codes", MySqlCol.id_primary(), cols = [MySqlCol("description","VARCHAR(140)")])
+		pass
