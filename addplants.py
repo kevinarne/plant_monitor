6cdd522a03541130f15f870dd6bd12d82e2719be
Kevin Arne <kevin.arne@gmail.com>
2021-05-23 21:35:34 -0700

    Added a basic script for adding plants to the plant table. Currently it just establishes a database connection and asks the user for the plant name and notes. Next step is to actually add that data to the table.

---
+++ addplants.py
@@ -0,0 +1,8 @@
+from util import sqlmanager
+#Add plants to database
+user_inp = input("Would you like to add a plant [y/n]? ")
+if user_inp == "y":
+    name = input("What is the plant's name? ")
+    notes = input("Please type any notes about the plant and press enter. ")
+    mngr = sqlmanager.MySqlManager("util/credentials", "lights")
+    print(name,notes)
