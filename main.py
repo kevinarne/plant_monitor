0119c3ae73d60f515888cadd81a5303128bac893
kevin.arne <kevin.arne@gmail.com>
2021-05-26 15:20:41 -0700

    Added a main.py to handle user-facing operations.

---
+++ main.py
@@ -0,0 +1,18 @@
+# Main script to manage all user-facing activities like adding new plants,
+# creating new event codes, adding manual events (like actual weights),
+# pulling up visualizations of the data, etc
+
+uinp = input("What would you like to do? ").lower()
+
+while True:
+	if uinp == "exit":
+		exit()
+	elif uinp == "menu":
+		print("Now I'll display the menu: ")
+	elif uinp == "add plant":
+		print("Adding plants")
+	elif uinp == "add weight":
+		print("Add weight")
+	else:
+		print("Sorry, that's not one of the menu options. To exit type exit")
+	uinp = input("What would you like to do? ").lower()
