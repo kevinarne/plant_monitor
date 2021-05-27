032d941f16389c7d0a09bdf5deb5b85320375379
kevin.arne <kevin.arne@gmail.com>
2021-05-27 10:40:50 -0700

    Added create tables function to sqlmanager to create all of the necessary tables in the database. Next step is to transition all of the values in the light_val table to the plant_events table. That's going to require a light sensor in the sensor table with my current 4 plants subscribed to it. Values will also need to be cast as integers, so no decimals.

--- util/sqlmanager.py
+++ util/sqlmanager.py
@@ -94,33 +94,32 @@ class Table:
 	def get_cols(self):
 		pass
 
+def createtables():
+	mngr = MySqlManager("credentials", "lights")
+	#Create database
+	#Create light_vals table
+	mngr.create_table("light_vals", MySqlCol.id_primary(), cols = [MySqlCol("val","INT"),
+		MySqlCol("datetime","VARCHAR(30)")])
+	#Create plants table
+	mngr.create_table("plants", MySqlCol.id_primary(), cols=[MySqlCol("nickname", "VARCHAR(40)"),
+		MySqlCol("notes","VARCHAR(200)")])
+	#Create plant_events table
+	mngr.create_table("plant_events", MySqlCol.id_primary(), cols = [MySqlCol("code","INT NOT NULL"),
+		MySqlCol("datetime","VARCHAR(30)"),
+		MySqlCol("val","INT"),
+		MySqlCol("plant","INT"),
+		MySqlCol("notes","VARCHAR(140)")])
+	#Create event_codes table
+	mngr.create_table("event_codes", MySqlCol.id_primary(), cols = [MySqlCol("description","VARCHAR(140)")])
+	#Create sensors table
+	mngr.create_table("sensors", MySqlCol.id_primary(), cols = [MySqlCol("description","VARCHAR(140)"),
+		MySqlCol("active","VARCHAR(1)"),
+		MySqlCol("type","INT"),
+		MySqlCol("schedule","VARCHAR(20)"),
+		MySqlCol("units","VARCHAR(20)"),
+		MySqlCol("subscribed","VARCHAR(30)")])
 
 if __name__ == "__main__":
-	mngr = MySqlManager("credentials", "lights")
 	user_inp = input("Would you like to create the needed databases and tables (y/n)?")
 	if user_inp == "y":
-		#Create database
-		#Create light_vals table
-		mngr.create_table("light_vals", MySqlCol.id_primary(), cols = [MySqlCol("val","INT"),
-			MySqlCol("datetime","VARCHAR(30)")])
-		#Create plants table
-		mngr.create_table("plants", MySqlCol.id_primary(), cols=[MySqlCol("nickname", "VARCHAR(40)"),
-			MySqlCol("notes","VARCHAR(200)")])
-		#Create plant_events table
-		mngr.create_table("plant_events", MySqlCol.id_primary(), cols = [MySqlCol("code","INT NOT NULL"),
-			MySqlCol("datetime","VARCHAR(30)"),
-			MySqlCol("val","INT"),
-			MySqlCol("plant","INT"),
-			MySqlCol("notes","VARCHAR(140)")])
-		#Create event_codes table
-		mngr.create_table("event_codes", MySqlCol.id_primary(), cols = [MySqlCol("description","VARCHAR(140)")])
-		#Create sensors table
-		mngr.create_table("sensors", MySqlCol.id_primary(), cols = [MySqlCol("description","VARCHAR(140)"),
-			MySqlCol("active","VARCHAR(1)"),
-			MySqlCol("type","INT"),
-			MySqlCol("schedule","VARCHAR(20)"),
-			MySqlCol("units","VARCHAR(20)"),
-			MySqlCol("subscribed","VARCHAR(30)")])
-		pass
-	#print("testing table addition")
-	#mngr.add_values("plants", ["nickname", "notes"],["Ficus", "Had for around 10 years. Bit of a drama queen."])
+		createtables()
