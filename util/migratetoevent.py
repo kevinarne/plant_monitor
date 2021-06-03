064a5c7638b47fa32f31a36028d4ccd2b9ef45ea
kevin.arne <kevin.arne@gmail.com>
2021-06-02 18:36:49 -0700

    Set the migratetoevent script to actually write to the plant_event database. The light readings are now being logged in the plant_event table. The light_val table is now deprecated.

--- util/migratetoevent.py
+++ util/migratetoevent.py
@@ -9,5 +9,5 @@ for entry in entries:
     for plant in plants:
         cols = ["code","datetime","val","plant","notes"]
         vals = [1,entry[2],entry[1],plant,"From sensor 1"]
-        #mngr.add_values("plant_events", cols, vals)
+        mngr.add_values("plant_events", cols, vals)
         print(vals)
