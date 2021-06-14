57e964cfafeb485b23a1dfc10245ca2f38b92ba5
Kevin Arne <kevin.arne@gmail.com>
2021-06-13 21:33:12 -0700

    Cleaned up sqlmanager.py to make it more general purpose. I would like to create a setup script that handles the table creation.

--- util/sqlmanager.py
+++ util/sqlmanager.py
@@ -54,7 +54,7 @@ class MySqlManager:
 			for col in cols:
 				query += ", " + col.to_str()
 		query += ", PRIMARY KEY(" + primary.name + "))"
-		print(query)
+		#print(query)
 		try:
 			self.execute_mysql(query,())
 		except:
@@ -86,37 +86,5 @@ class MySqlCol:
 	def id_primary():
 		return MySqlCol("id", "INT NOT NULL AUTO_INCREMENT")
 
-class Table:
-	def __init__(self, primary, cols):
-		self.primary = MySqlCol.id_primary()
-		self.cols = []
-	def get_primary(self):
-		pass
-	def get_cols(self):
-		pass
-
-def createtables(dbname):
-	mngr = MySqlManager(dbname)
-	#Create plants table
-	mngr.create_table("plants", MySqlCol.id_primary(), cols=[MySqlCol("nickname", "VARCHAR(40)"),
-		MySqlCol("notes","VARCHAR(200)")])
-	#Create plant_events table
-	mngr.create_table("plant_events", MySqlCol.id_primary(), cols = [MySqlCol("code","INT NOT NULL"),
-		MySqlCol("datetime","VARCHAR(30)"),
-		MySqlCol("val","INT"),
-		MySqlCol("plant","INT"),
-		MySqlCol("notes","VARCHAR(140)")])
-	#Create event_codes table
-	mngr.create_table("event_codes", MySqlCol.id_primary(), cols = [MySqlCol("description","VARCHAR(140)")])
-	#Create sensors table
-	mngr.create_table("sensors", MySqlCol.id_primary(), cols = [MySqlCol("description","VARCHAR(140)"),
-		MySqlCol("active","VARCHAR(1)"),
-		MySqlCol("type","INT"),
-		MySqlCol("schedule","VARCHAR(20)"),
-		MySqlCol("units","VARCHAR(20)"),
-		MySqlCol("subscribed","VARCHAR(30)")])
-
 if __name__ == "__main__":
-	user_inp = input("Would you like to create the needed databases and tables (y/n)?")
-	if user_inp == "y":
-		createtables("lights")
+	pass
