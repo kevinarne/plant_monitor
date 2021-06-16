f7c68bbb78a71ad83963b1f1648c437117db76d7
kevin.arne <kevin.arne@gmail.com>
2021-06-16 14:35:59 -0700

    Got a working version of mysqli_connect.php.

--- web/mysqli_connect.php
+++ web/mysqli_connect.php
@@ -1,9 +1,13 @@
 <?php
-require_once('creds.php');
+require_once('../../creds.php');
 
-$dbc = @mysqli_connect(HOST_NAME, USER_NAME, PASSWORD, DN_NAME) OR die('Could not connect to MySQL ' . mysqli_connect_error());
-$query = "SELECT * FROM event_codes;"
+$dbc = @mysqli_connect(HOST_NAME, USER_NAME, PASSWORD, DB_NAME) OR die('Could not connect');
+$query = "SELECT * FROM event_codes;";
 
-$response = @mysqli_query($dbc, $query);
-echo '<h1>Query succeeded</h>'
+$response = @mysqli_query($dbc,$query);
+
+if($response)
+{
+	echo "Test4";
+}
 ?>
