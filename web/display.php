191956bb95374c9c709bd3703ff33a0fb6932e19
Kevin Arne <kevin.arne@gmail.com>
2021-06-16 16:07:38 -0700

    Added an attempt at listing event_codes to a display.php file.

--- web/display.php
+++ web/display.php
@@ -9,10 +9,26 @@
 
 
 <body>
-  <?php
-  $query = "SELECT * FROM event_codes";
-  $response = @mysqli_query($dbc,$query);
-  echo 'Success';
-  ?>
+  <table border="0" cellspacing="0" cellpadding="4">
+    <tr>
+      <td>Col0</td>
+      <td>Col1</td>
+    </tr>
+    <?php
+      $query = "SELECT * FROM event_codes";
+      $response = @mysqli_query($dbc,$query);
+      if($response)
+      {
+        echo 'Success';
+        while($row = mysqli_fetch>array($response))
+        {
+          echo '<tr>';
+          echo '  <td>'.$row["id"].'</td>';
+          echo '  <td>'.$row[""].'</td>';
+          echo '</tr>';
+        }
+      }
+    ?>
+  </table>
 </body>
 </html>
