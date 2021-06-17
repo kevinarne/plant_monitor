ee198c85f5c3ab2f20e2f6d378d256f298026d14
kevin.arne <kevin.arne@gmail.com>
2021-06-17 09:33:32 -0700

    Tested pulling the data from _POST in weightadded.php. Next step is to have it add the data to the database.

--- web/weightadded.php
+++ web/weightadded.php
@@ -1,3 +1,50 @@
+<html>
+	<head>
+		<title>Weight Added</title>
+	</head>
+	<body>
+		<?php
+			if(isset($_POST['submit']))
+			{
+				$data_missing = array();
+				$f_plant = $_POST['plant'];
+				if(empty($_POST['date']))
+				{
+					$data_missing[] = 'date';
+				}
+				else
+				{
+					$f_date = $_POST['date'];
+					echo $f_date;
+				}
+
+				if(empty($_POST['val']))
+				{
+					$data_missing[] = 'value';
+				}
+				else
+				{
+					$f_val = $_POST['val'];
+					echo $f_val;
+				}
+
+				if(empty($_POST['notes']))
+				{
+					$f_notes = "";
+					echo $f_val;
+				}
+				else
+				{
+					$f_notes = $_POST['notes'];
+				}
+			}
+			echo '<h1>success</h1>';
+		?>
+
+
+	</body>
+
 <?php
 	echo '<h1>Nothing here<h1>';
 ?>
+</html>
