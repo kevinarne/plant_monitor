17531665fe5061ab8e4910662b4c82454f0dc7e7
kevin.arne <kevin.arne@gmail.com>
2021-06-16 20:43:09 -0700

    addweight.php displays proper plant options now. Created a file for handling actually putting the data in the database.

--- web/addweight.php
+++ web/addweight.php
@@ -4,10 +4,10 @@
   </head>
 
   <body>
-    <form action="http://localhost/weightadded.php" method="post">
+    <form action="http://10.0.0.123/weightadded.php" method="post">
       <select name="plant" id="plant">
       <?php
-        require_once('../../mysqli_connect.php');
+        require_once('../mysqli_connect.php');
         # Pull the possible plants to add to the drop down
         $query = "SELECT id, nickname FROM plants";
         $response = @mysqli_query($dbc, $query);
@@ -16,13 +16,13 @@
           # Plant drop down
           while($row = mysqli_fetch_array($response))
           {
-              echo '<option value="'.$row['id'].'">'.$row['nickname'].'</option>'
+              echo '<option value='.$row["id"].'>'.$row["nickname"].'</option>';
           }
         }
       ?>
       </select>
       <p>Date:<input type="date" name="date"/></p>
-      <p>Value:<input type="number" name="val"/></p>
+      <p>Value:<input type="number" name="val" step="any"/></p>
       <p>Notes:<input type="text" name="notes"/></p>
       <p><input type="submit" name="submit" value="Submit"/></p>
     </form>
