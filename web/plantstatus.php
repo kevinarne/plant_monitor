9f81a777cfd98952079e05d8ac85f6fedabee438
kevin.arne <kevin.arne@gmail.com>
2021-06-24 12:43:57 -0700

    Implemented plant status in php so I can now access the plant statuses from my phone easily. This page details when each plant was last watered, at what weight, and what the current weight is.

--- web/plantstatus.php
+++ web/plantstatus.php
@@ -14,6 +14,7 @@
       <td>Plant</td>
       <td>Current Weight</td>
       <td>Last Watering</td>
+      <td>Weight at Last Watering</td>
     </tr>
     <?php
       $query = "SELECT * FROM plant_events WHERE code=2";
@@ -22,25 +23,56 @@
 
       if($events && $plants)
       {
-        // Create display array from the plants table with empty values
-          // $display = array(id => array(name =>, waterdate=>,waterweight=>,mostrecent=>));
-        // Create temporary variables for each plant to store yesterday's date, yesterday's weight, and today's weight
-          // $temp = array(id => array(yesterweight =>, yesterdate=>);
-        // Traverse the events array
+	$display = array();
+        $temp = array();
+
+	while($row = mysqli_fetch_array($plants))
+	{
+		// Create display array from the plants table with empty values
+		$display [$row["id"]] = array(
+			"nickname"=>$row["nickname"],
+			"waterdate"=>"",
+			"waterweight"=>"",
+			"curweight"=>""
+			);
+		// Create temporary variables for each plant to store yesterday's date and yesterday's weight
+		$temp [$row["id"]] = array(
+			"yesterweight"=>0,
+			"yesterdate"=>""
+			);
+	}
+	// Traverse the events array
+	// This next bit is practically unreadable, needs refactoring
+	while($row = mysqli_fetch_array($events))
+	{
           // Determine which plant it pertains to
           // If there's a value for yesterday's date, and yesterday's weight
-            // Compare today's weight to yesterday's and store the date and weight in the watering event if today's is greater than yesterday's
-            // Store today's weight in display array
+	  if($temp[$row["plant"]]["yesterweight"]!="")
+	  {
+	    // Compare today's weight to yesterday's and store the date and weight in the watering event if today's is greater than yesterday's
+	    if($temp[$row["plant"]]["yesterweight"] < $row["val"])
+	    {
+	      // Store today's weight in display array
+	      $display[$row["plant"]]["waterdate"] = $temp[$row["plant"]]["yesterdate"];
+	      $display[$row["plant"]]["waterweight"] = $temp[$row["plant"]]["yesterweight"];
+	    }
+	  }
+	  $display[$row["plant"]]["curweight"] = $row["val"];
           // Store today's weight and date in yesterday's
-        while($row = mysqli_fetch_array($events))
+	  $temp[$row["plant"]]["yesterweight"] = $row["val"];
+	  $temp[$row["plant"]]["yesterdate"] = $row["datetime"];
+        }
+
+	foreach($display as $plantrow)
         {
           echo '<tr>';
-          echo '  <td>'.$row["plant"].'</td>';
-          echo '  <td>'.$row["val"].'</td>';
-          echo '  <td>'.0.'</td>';
-          echo '</tr>';
+          echo '  <td>'.$plantrow["nickname"].'</td>';
+          echo '  <td>'.$plantrow["curweight"].'</td>';
+          echo '  <td>'.$plantrow["waterdate"].'</td>';
+          echo '  <td>'.$plantrow["waterweight"].'</td>';
+	  echo '</tr>';
         }
-      }
+    }
     ?>
   </table>
 </body>
