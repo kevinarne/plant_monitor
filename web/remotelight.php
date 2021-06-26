1c079268da1c43d71406b28babc43e38ed6e5adc
kevin.arne <kevin.arne@gmail.com>
2021-06-25 21:38:43 -0700

    Fleshed out php page for adding a light sensor's readings to the database. Currently iterates through the appropriate plants and adds dummy data. Needs to be changed to add real data.

--- web/remotelight.php
+++ web/remotelight.php
@@ -10,6 +10,7 @@
   if(empty($_POST['val']))
   {
       // Exit error
+      //exit();
   }
   else
   {
@@ -20,6 +21,7 @@
   if(empty($_POST['sid']))
   {
     // Exit error
+    //exit();
   }
   else
   {
@@ -40,11 +42,39 @@
   if(empty($_POST['eid']))
   {
     //exit
+    //exit();
   }
   else
   {
     $f_eid = $_POST['eid'];
     echo $f_eid;
   }
+  $f_sid = 1;
+  require_once('../mysqli_connect.php');
   // Get plants subscribed to that sensor
+  $sensorquery = "SELECT subscribed FROM sensors WHERE id=".$f_sid;
+  $sensorresponse = @mysqli_query($dbc, $sensorquery);
+  $subscribed = mysqli_fetch_array($sensorresponse);
+  $subarr = explode(",", $subscribed[0]);
+  foreach($subarr as $plant)
+  {
+    $query = "INSERT INTO plant_events (code, datetime, val, plant, notes) VALUES (?,?,?,?,?)";
+    $code = 1;
+    $stmt = mysqli_prepare($dbc, $query);
+    $datetimetext = "datetime text";
+    $thisplant = $plant[0];
+    $notes = "test";
+    $val = 2;
+
+    mysqli_stmt_bind_param(
+      $stmt,
+      "isiis",
+      $code,
+      $datetimetext,
+      $val,
+      intval($thisplant),
+      $notes
+      );
+    mysqli_stmt_execute($stmt);
+  }
     // Add sensor values for each plant
