15c0a3e809945401248e28bd1b50165faebaae63
kevin.arne <kevin.arne@gmail.com>
2021-06-26 22:05:11 -0700

    Adding light data to the database remotely isn't currently working. Appears to be failing at the line where the data is actually added to the database.

--- web/remotelight.php
+++ web/remotelight.php
@@ -10,7 +10,7 @@
   if(empty($_POST['val']))
   {
       // Exit error
-      //exit();
+      exit();
   }
   else
   {
@@ -21,7 +21,7 @@
   if(empty($_POST['sid']))
   {
     // Exit error
-    //exit();
+    exit();
   }
   else
   {
@@ -31,7 +31,7 @@
 
   if(empty($_POST['dt']))
   {
-    //$f_dt = now;
+    $f_dt = date('Y-m-dTH:m:s');
   }
   else
   {
@@ -42,39 +42,35 @@
   if(empty($_POST['eid']))
   {
     //exit
-    //exit();
+    exit();
   }
   else
   {
     $f_eid = $_POST['eid'];
     echo $f_eid;
   }
-  $f_sid = 1;
+
   require_once('../mysqli_connect.php');
   // Get plants subscribed to that sensor
   $sensorquery = "SELECT subscribed FROM sensors WHERE id=".$f_sid;
   $sensorresponse = @mysqli_query($dbc, $sensorquery);
   $subscribed = mysqli_fetch_array($sensorresponse);
   $subarr = explode(",", $subscribed[0]);
+
   foreach($subarr as $plant)
   {
-    $query = "INSERT INTO plant_events (code, datetime, val, plant, notes) VALUES (?,?,?,?,?)";
-    $code = 1;
-    $stmt = mysqli_prepare($dbc, $query);
-    $datetimetext = "datetime text";
-    $thisplant = $plant[0];
-    $notes = "test";
-    $val = 2;
+    //$query = "INSERT INTO plant_events (code, datetime, val, plant, notes) VALUES (?,?,?,?,?)";
+    //$stmt = mysqli_prepare($dbc, $query)
+    //$notes = "test";
 
-    mysqli_stmt_bind_param(
-      $stmt,
-      "isiis",
-      $code,
-      $datetimetext,
-      $val,
-      intval($thisplant),
-      $notes
-      );
-    mysqli_stmt_execute($stmt);
+    //mysqli_stmt_bind_param(
+     // $stmt,
+      //"isiis",
+      //intval($f_eid),
+      //$f_dt,
+      //intval($f_val),
+      //intval($plant),
+      //$notes
+      //);
+    //mysqli_stmt_execute($stmt);
   }
-    // Add sensor values for each plant
