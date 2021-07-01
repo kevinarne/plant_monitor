74b23699b20d118903de72e8734853b24ce31f99
Kevin Arne <kevin.arne@gmail.com>
2021-06-30 18:26:19 -0700

    Updated light sensor script to allow for configurable sensor and event ids.

--- ard/wllight/wllight.ino
+++ ard/wllight/wllight.ino
@@ -2,14 +2,19 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
+#define POLLING_INTERVAL 60000 // In milliseconds
+
 #define CDS_PIN 34
+
+// Use 0 for testing to make it easy to remove from the database
+int sensor_id = 0;
+int event_id = 0;
 
 String serverName = "http://10.0.0.123/remotelight.php";
 
 void setup() 
 {
   Serial.begin(115200);
-  //pinMode(CDS_PIN, INPUT);
   
   WiFi.begin(NETWORK, PASS);
   while(WiFi.status() != WL_CONNECTED)
@@ -25,16 +30,20 @@ void loop()
   if(WiFi.status() == WL_CONNECTED)
   {
     HTTPClient http;
-    uint16_t val = analogRead(CDS_PIN);
+    uint16_t val = readSensor();
     Serial.print("Value: ");
     Serial.println(val);
-    String query = serverName + "?val="+val+"&sid=1&eid=0";
+    String query = 
+      serverName + 
+      "?val=" + val + 
+      "&sid=" + sensor_id + 
+      "&eid=" + event_id;
     
     http.begin(query.c_str());
     int httpResponse = http.GET();
-    //Serial.println(httpResponse);
+
     http.end();
-    delay(60000);
+    delay(POLLING_INTERVAL);
   }
 }
 
