dc1c2cc5e3beb86059616115673401ecf252f606
Kevin Arne <kevin.arne@gmail.com>
2021-07-05 15:32:38 -0700

    Made some minor tweaks to the sensor checker script. Currently the serial light sensor isn't being added to the database correctly. Need to design a PCB to make it easier to wire up new light sensors.

--- util/checksensors.py
+++ util/checksensors.py
@@ -1,13 +1,22 @@
 from sqlmanager import MySqlManager
+import datetime
 
 mngr = MySqlManager("lights")
+
+SENSOR_ACTIVE_COL = 2
+ID_COL = 0
+PLANT_EVENT_DATETIME_COL = 2
 
 # Get the list of sensors from the database
 sensors = mngr.export_table("sensors")
 
 # For each sensor, find the most recent entry
 for row in sensors:
-    if row[2] == "y":
-        query = "SELECT * FROM plant_events WHERE source='s" + str(row[0]) + "' ORDER BY id DESC LIMIT 1"
-        mostrecent = mngr.execute_mysql(query,[])
-        print(mostrecent)
+    if row[SENSOR_ACTIVE_COL] == "y":
+        query = "SELECT * FROM plant_events WHERE source='s" + str(row[ID_COL]) + "' ORDER BY id DESC LIMIT 1"
+        # Get the first row (only row) and the datetime column
+        rawdata = mngr.execute_mysql(query,[])[0][PLANT_EVENT_DATETIME_COL]
+        # Create a datetime object from the modified datetime string (to remove the "PD" from "PDT")
+        mostrecent = datetime.datetime.fromisoformat(rawdata[:10] + rawdata[12:])
+        today = datetime.datetime.today()
+        print("Time since last reported:", today - mostrecent)
