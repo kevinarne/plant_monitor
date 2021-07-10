cd2ceee888bc087b404fe5b3fef87fc34fb3b220
Kevin Arne <kevin.arne@gmail.com>
2021-07-10 12:51:33 -0700

    Running into some incompatibilities between the ways datetimes are being encoded by the manual records and weight sensor. I can tweak the existing data to work with the current script, but I should also change the way the data is encoded by the weight sensor.

--- util/visualweight.py
+++ util/visualweight.py
@@ -3,9 +3,18 @@ import sqlmanager
 
 mngr = sqlmanager.MySqlManager("lights")
 # pull out weight sensor data
-senseData = mngr.execute_mysql("SELECT datetime, val FROM plant_events WHERE code=8 AND plant=2", [])
+senseData = mngr.execute_mysql("SELECT datetime, val FROM plant_events WHERE code=8 AND plant=2 LIMIT 10", [])
 sensex = [entry[0] for entry in senseData]
 sensey = [int(entry[1]) / 100.0 for entry in senseData]
+print(sensex[0], "Length:", len(sensex[0]))
+
+
+manualData = mngr.execute_mysql("SELECT datetime, val FROM plant_events WHERE code=2 AND plant=2 LIMIT 10", [])
+manualx = [entry[0] for entry in manualData]
+manualy = [int(entry[1]) / 100.0 for entry in manualData]
+print(manualx[0], "Length:", len(manualx[0]))
+
 
 plt.plot(sensex, sensey)
-plt.show()
+plt.plot(manualx, manualy)
+#plt.show()
