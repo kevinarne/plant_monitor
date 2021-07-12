615e1a59c6e8c0ddf2dee8587cbe31f9c805d4f5
Kevin Arne <kevin.arne@gmail.com>
2021-07-11 20:56:26 -0700

    Added some datetime string handling adjustments to the checksensors script to handle discrepencies between the time reporting of different sensors.

--- util/checksensors.py
+++ util/checksensors.py
@@ -14,9 +14,13 @@ sensors = mngr.export_table("sensors")
 for row in sensors:
     if row[SENSOR_ACTIVE_COL] == "y":
         query = "SELECT * FROM plant_events WHERE source='s" + str(row[ID_COL]) + "' ORDER BY id DESC LIMIT 1"
-        # Get the first row (only row) and the datetime column
-        rawdata = mngr.execute_mysql(query,[])[0][PLANT_EVENT_DATETIME_COL]
-        # Create a datetime object from the modified datetime string (to remove the "PD" from "PDT")
-        mostrecent = datetime.datetime.fromisoformat(rawdata[:10] + rawdata[12:])
+        # Get the first row (only row) and the datetime column, removing anyhing after the decimal
+        rawdata = mngr.execute_mysql(query,[])[0][PLANT_EVENT_DATETIME_COL].split(".")[0]
+
+        # Remove the PD from PDT if it exists
+        if len(rawdata) == 21:
+            rawdata = rawdata[:10] + rawdata[12:]
+
+        mostrecent = datetime.datetime.fromisoformat(rawdata)
         today = datetime.datetime.today()
         print("Time since last reported:", today - mostrecent)
