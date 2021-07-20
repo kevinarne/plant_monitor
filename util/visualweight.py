a08c87488e8eed7e3be5d60115dc1c01c2207f16
Kevin Arne <kevin.arne@gmail.com>
2021-07-20 16:27:11 -0700

    Fixed time overlap problem in visualization script. X values were not previously converted to datetime objects. Created a short function to narrow the range of dates displayed to the start of the sensor readings.

--- util/visualweight.py
+++ util/visualweight.py
@@ -1,33 +1,63 @@
 import matplotlib.pyplot as plt
-from matplotlib.dates import date2num, DateFormatter
 import datetime as dt
 import sqlmanager
 import decouple
 
+# These are used as indices, which suggests I should've used a dictionary
+DATETIME_IND = 0
+VAL_IND = 1
+
 mngr = sqlmanager.MySqlManager(decouple.config('DB_NAME'))
 
 def filter_zeros(data, tolerance=100):
-    return [entry for entry in data if int(entry[1]) > tolerance]
+    return [entry for entry in data if int(entry[VAL_IND]) > tolerance]
 
-def has_pdt(string):
-    if "PDT" in str:
-        return True
-    return False
+def fix_iso(data):
+    new_data = []
+    for row in data:
+        entry = list(row)
+        entry[DATETIME_IND] = dt.datetime.fromisoformat(entry[DATETIME_IND][:19])
+        new_data.append(entry)
+    return new_data
+
+def adjust_start(data, strt=None, end=None):
+    # Start at the beginning of the data and go until the first index after strt
+    start_ind = 0
+    if strt:
+        for _ in range(len(data) - 1):
+            if data[start_ind][DATETIME_IND] > strt:
+                break
+            else:
+                start_ind += 1
+
+    # Start at the end and go until the first date that's before end
+    end_ind = len(data) - 1
+    if end:
+        for _ in range(len(data) - 1):
+            if data[end_ind][DATETIME_IND] < end:
+                break
+            else:
+                end_ind -= 1
+    return data[start_ind : end_ind + 1]
 
 if __name__ == "__main__":
-    senseData = mngr.execute_mysql("SELECT datetime, val FROM plant_events WHERE code=8 AND plant=2", [])
+    sense_data = mngr.execute_mysql('SELECT datetime, val FROM plant_events WHERE code=8 AND plant=2', [])
 
-    senseData = filter_zeros(senseData)
+    sense_data = fix_iso(filter_zeros(sense_data))
 
-    sensex = [dt.datetime.fromisoformat(entry[0]).timestamp() for entry in senseData]
-    sensey = [int(entry[1]) / 100.0 for entry in senseData]
+    sensex = [entry[DATETIME_IND] for entry in sense_data]
+    sensey = [int(entry[VAL_IND]) / 100.0 for entry in sense_data]
 
-    manualData = mngr.execute_mysql("SELECT datetime, val FROM plant_events WHERE code=2 AND plant=2", [])
-    manualx = [dt.datetime.fromisoformat(entry[0]).timestamp() for entry in manualData]
-    manualy = [int(entry[1]) / 100.0 for entry in manualData]
+    manual_data = mngr.execute_mysql('SELECT datetime, val FROM plant_events WHERE code=2 AND plant=2', [])
+
+    manual_data = adjust_start(fix_iso(manual_data), strt=sensex[0])
+
+    manualx = [entry[DATETIME_IND] for entry in manual_data]
+    manualy = [int(entry[VAL_IND]) / 100.0 for entry in manual_data]
 
     plt.plot(sensex, sensey)
-    plt.xlabel("Time")
+    plt.xlabel('Time')
+    plt.xticks(rotation=30, ha='right')
     plt.ylabel('Weight (oz)')
     plt.plot(manualx, manualy)
     plt.show()
