aab1ef695c07bc56af0ff92a5b22e098fbebb6b0
Kevin Arne <kevin.arne@gmail.com>
2021-07-21 11:43:57 -0700

    Updated check sensors script to send emails when it has been more than a day since the sensor checked in.

--- util/checksensors.py
+++ util/checksensors.py
@@ -1,7 +1,11 @@
 from sqlmanager import MySqlManager
 import datetime
+from decouple import config
+import smtplib, ssl
 
-mngr = MySqlManager("lights")
+port = 465
+
+mngr = MySqlManager(config('DB_NAME'))
 
 SENSOR_ACTIVE_COL = 2
 ID_COL = 0
@@ -10,17 +14,28 @@ PLANT_EVENT_DATETIME_COL = 2
 # Get the list of sensors from the database
 sensors = mngr.export_table("sensors")
 
+sensor_status_string = ""
+
 # For each sensor, find the most recent entry
 for row in sensors:
     if row[SENSOR_ACTIVE_COL] == "y":
+        # Query the database for the most recent entry from each sensor
         query = "SELECT * FROM plant_events WHERE source='s" + str(row[ID_COL]) + "' ORDER BY id DESC LIMIT 1"
-        # Get the first row (only row) and the datetime column, removing anyhing after the decimal
-        rawdata = mngr.execute_mysql(query,[])[0][PLANT_EVENT_DATETIME_COL].split(".")[0]
 
-        # Remove the PD from PDT if it exists
-        if len(rawdata) == 21:
-            rawdata = rawdata[:10] + rawdata[12:]
+        # Convert the datetime column of that entry to the proper iso string
+        iso_string = mngr.execute_mysql(query,[])[0][PLANT_EVENT_DATETIME_COL].split(".")[0]
 
-        mostrecent = datetime.datetime.fromisoformat(rawdata)
+        most_recent = datetime.datetime.fromisoformat(iso_string)
         today = datetime.datetime.today()
-        print("Time since last reported:", today - mostrecent)
+
+        print(f"Time since sensor {row[ID_COL]} last reported: {today - most_recent}")
+
+        if today - most_recent > datetime.timedelta(days=1):
+            sensor_status_string += f"s{row[ID_COL]}\n"
+
+if len(sensor_status_string) > 0:
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(config('EMAIL_USER'), config('EMAIL_PASS'))
+        message = sensor_status_string
+        server.sendmail(config('EMAIL_USER'), config('MAIN_EMAIL'), "Subject: Sensor down\n\nThe following sensors have not reported in over a day:\n"+sensor_status_string)
