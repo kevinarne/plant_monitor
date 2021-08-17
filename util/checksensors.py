17d9c21ca20961e3f76795eadc8f5b30db97b962
Kevin Arne <kevin.arne@gmail.com>
2021-08-17 10:50:26 -0700

    Added some extra comments to checksensors.py to clarify how it works.

--- util/checksensors.py
+++ util/checksensors.py
@@ -3,6 +3,7 @@ import datetime
 from decouple import config
 import smtplib, ssl
 
+# This is the smtp port used for emails
 port = 465
 
 mngr = MySqlManager(config('DB_NAME'))
@@ -25,17 +26,21 @@ for row in sensors:
         # Convert the datetime column of that entry to the proper iso string
         iso_string = mngr.execute_mysql(query,[])[0][PLANT_EVENT_DATETIME_COL].split(".")[0]
 
+        # Get the most recent reported date and today's date
         most_recent = datetime.datetime.fromisoformat(iso_string)
         today = datetime.datetime.today()
 
         print(f"Time since sensor {row[ID_COL]} last reported: {today - most_recent}")
 
+        # If more than one day has elapsed, add the current sensor to the list of offline ones
         if today - most_recent > datetime.timedelta(days=1):
             sensor_status_string += f"s{row[ID_COL]}\n"
 
+# Check whether any sensors were reported as offline
 if len(sensor_status_string) > 0:
     context = ssl.create_default_context()
+    # Send an email to the user
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(config('EMAIL_USER'), config('EMAIL_PASS'))
         message = sensor_status_string
-        server.sendmail(config('EMAIL_USER'), config('MAIN_EMAIL'), "Subject: Sensor down\n\nThe following sensors have not reported in over a day:\n"+sensor_status_string)
+        server.sendmail(config('EMAIL_USER'), config('MAIN_EMAIL'), "Subject: Sensor down\n\nThe following sensors have not reported in over a day:\n" + sensor_status_string)
